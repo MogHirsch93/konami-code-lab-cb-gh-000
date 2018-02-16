3c6edb0b2739284f64e961033bd2e16d460ef765
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-16 16:59:16 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,7 +3,6 @@ describe('index', () => {
 
   function triggerKeyDown(which) {
     const keyboardEvent = document.createEvent("KeyboardEvent")
-    console.log(which)
     keyboardEvent.initKeyboardEvent(
       'keydown',
       true,
