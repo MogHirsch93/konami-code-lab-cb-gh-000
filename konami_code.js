37dbc4dd03f33c5d5d8874d3358a5163e54f213b
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-16 16:37:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,12 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // Write your JavaScript code inside the init() function
   let input = document.body.querySelector('input')
-
+  let index = 0;
+  
   input.addEventListener('keydown', function(event){
-    let index = 0;
+
     let key = parseInt(e.which);
 
     if(event.which === code[index]){
