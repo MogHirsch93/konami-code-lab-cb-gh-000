524702332dd90f7c2c3e64b172c6132fed53c993
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-16 16:49:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,20 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // Write your JavaScript code inside the init() function
-    let index = 0;
-    
     document.body.addEventListener('keydown', function(event){
 
     let key = parseInt(e.which);
 
     for(let i = 0; i < code.length; i++){
-    if(key === code[index]){
-      index++;
-    }if(index === code.length){
+    if(key === code[i]){
+      i++;
+    }if(i === code.length){
       alert("You only went and did it. You are the king of the konami code!");
-      index = 0;
+      i = 0;
     }else{
-      index = 0;
+      i = 0;
       }
     }
   })
