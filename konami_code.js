7f15af20175a3fd24c34bfdeff08bbdcaef3b91c
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-16 16:24:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  let input = document.querySelector('input')
+  let input = document.body.querySelector('input')
 
   input.addEventListener('keydown', function(event){
 
@@ -18,5 +18,4 @@ function init() {
       index = 0;
     }
   })
-
 }
